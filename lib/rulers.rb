9bf349e162267e1381bd2565caf5e98e66478b84
remotezen <remotezen@bell.net>
2016-05-21 16:42:43 -0400

    NEW HEAD

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,7 +2,7 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
-      `echo debug > ./log/debug.txt`
+      `echo debug > debug.txt`
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
     end
