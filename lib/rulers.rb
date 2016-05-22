ca5df1ae067cafacc5e772b2a16022707497f478
remotezen <remotezen@bell.net>
2016-05-22 17:35:04 -0400

    remove gem

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,13 @@ require 'rulers/routing'
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return[404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO' == '/']
+        text = controller.send('/quotes/a_quote')
+        [200, {'Content-Type'=> 'text/html'}, [text]]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
