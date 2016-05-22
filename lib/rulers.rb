632bef8f27a905c90ebf746dc09f1317323e1ba4
remotezen <remotezen@bell.net>
2016-05-22 17:08:55 -0400

    removed gem

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,11 @@
 require "rulers/version"
-require 'rulers/array'
 require 'rulers/routing'
+
 module Rulers
   class Application
     def call(env)
       klass, act = get_controller_and_action(env)
-      controller = Klass.new(env)
+      controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
       [text]]
